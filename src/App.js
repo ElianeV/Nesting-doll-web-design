574efad446350ebc852858cdaaf84ab52970a82c
ElianeV <eliane5@protonmail.com>
2022-08-03 10:41:45 +0100

    intro animation finished

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import FullPage, {
   Fullpage,
 } from "@ap.cx/react-fullpage";
 import BigEyebrow from "./images/BigEyebrow.png";
+import NestingDoll from "./images/NestingDoll.png";
+import Signature from "./images/Signature.png";
 
 function App() {
   return (
@@ -13,9 +15,16 @@ function App() {
       <FullPageSections>
         <FullpageSection style={{ width: "100vw", height: "100vh" }}>
           <div className="FullScreen Intro">
-            <h1>Intro</h1>
-            <div className="BigEyebrow">
-              <img src={BigEyebrow} alt="eyebrow" />
+            <div className="NestingDoll">
+              <img src={NestingDoll} alt="nestingdoll" />
+            </div>
+            <div className="Eyebrows">
+              <img id="BigEyebrow" src={BigEyebrow} alt="eyebrow" />
+              <img id="MidEyebrow" src={BigEyebrow} alt="eyebrow" />
+              <img id="SmallEyebrow" src={BigEyebrow} alt="eyebrow" />
+            </div>
+            <div className="Signature">
+              <img id="Signature" src={Signature} alt="signature" />
             </div>
           </div>
         </FullpageSection>
@@ -25,8 +34,8 @@ function App() {
           </div>
         </FullpageSection>
         <FullpageSection style={{ width: "100vw", height: "100vh" }}>
-          <div className="Projects-top-doll"></div>
-          <div className="Projects-bottom-doll"></div>
+          {/* <div className="Projects-top-doll"></div>
+          <div className="Projects-bottom-doll"></div> */}
           <div className="FullScreen Projects">
             <p>Projects</p>
           </div>
