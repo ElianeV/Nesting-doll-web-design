f69e519658cb64fb0a299e41c2b38b44939811f1
Eliane V <eliane.vdc@protonmail.com>
2022-09-29 15:04:33 +0100

    remove form

--- src/App.js
+++ src/App.js
@@ -191,33 +191,7 @@ function App() {
         </div>
       </div>
       <div style={{ width: "100vw", height: "100vh" }}>
-        <div id="ContactPage" className="FullScreen Contact">
-          <form
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            Content-Type="application/x-www-form-urlencoded"
-          >
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message"></textarea>
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        </div>
+        <div id="ContactPage" className="FullScreen Contact"></div>
       </div>
     </>
   );
